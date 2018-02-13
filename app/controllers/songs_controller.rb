6d3addbd0de47268bf7627c4b8fd6bc8418d9820
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-13 10:19:47 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,14 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug]).first
+    # binding.pry
+    erb :'songs/show'
+  end
+
+end
