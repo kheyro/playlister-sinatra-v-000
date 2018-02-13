3a9e7c5df2caa1b632b456021e2637b1601cba73
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-13 11:37:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  use Rack::Flash
-  
   get '/' do
     erb :index
   end
