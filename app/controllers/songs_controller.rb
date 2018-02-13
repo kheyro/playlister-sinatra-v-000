5f5c78991bb32da38f038dd0d87deb7b14f27dda
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-13 11:54:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,15 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song = Song.update(params[:song])
+    @song.artist = Artist.find_by(name: params[:artist_name])
+    flash[:message] = "Successfully created song."
+    
+    redirect to("/songs/#{@song.slug}")
+  end
+  
   get '/songs/new' do
     @artists = Artist.all
     @genres = Genre.all
