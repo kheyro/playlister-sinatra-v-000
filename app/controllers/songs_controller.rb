afe13d261cf8b8cb8ceb78fc738dfd1c08b40d6f
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-13 11:26:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,10 +17,10 @@ class SongsController < ApplicationController
   end
 
   post :'/songs' do
-    Artist.create(name: "Person with a Face")
+    # Artist.create(name: "Person with a Face")
     @song = Song.create(params[:song])
     # if !params[:artist_name].empty?
-      @song.artist = Artist.create(name: "Person with a Face")
+      # @song.artist = Artist.create(name: "Person with a Face")
     # end
     binding.pry
     @song.save
